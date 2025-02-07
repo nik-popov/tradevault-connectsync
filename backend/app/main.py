2c0bec66dbd4439e50516a4ff397101e57c1d240
cobaltdata <nik@cobaltdata.net>
2025-02-07 05:19:32 +0000

    server

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,10 +21,8 @@ app = FastAPI(
 )
 
 # Set all CORS enabled origins
-if settings.all_cors_origins:
-    app.add_middleware(
+app.add_middleware(
         CORSMiddleware,
-#        allow_origins=settings.all_cors_origins,
 	allow_origins=["*"], 
        	allow_credentials=True,
         allow_methods=["*"],
