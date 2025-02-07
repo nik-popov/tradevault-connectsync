a6b018ea8370a3be3de724491de2afe99ecaa7a6
cobalt admin <nik@cobaltdata.net>
2025-02-07 18:22:35 -0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,14 +19,11 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
-
-# Set all CORS enabled origins
 app.add_middleware(
-        CORSMiddleware,
-	allow_origins=["*"], 
-       	allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow only your frontend
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],  # Ensure OPTIONS is included
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix=settings.API_V1_STR)
