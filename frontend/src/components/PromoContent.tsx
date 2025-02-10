63bf92f43095c0ba33b2992258df8f3561b84816
cobalt admin <nik@cobaltdata.net>
2025-02-10 22:09:16 +0000

    dash

--- frontend/src/components/PromoContent.tsx
+++ frontend/src/components/PromoContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Button, VStack } from '@/components/ui/Card';
+import { Box, Text, Button, VStack } from "@chakra-ui/react";
 import { createFileRoute } from '@tanstack/react-router';
 
 const PromoContent = () => {
