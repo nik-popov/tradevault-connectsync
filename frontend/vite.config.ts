63b724b9a02c1695b4eb8c0fb73343094e8dc737
cobalt admin <nik@cobaltdata.net>
2025-02-10 21:56:37 +0000

    dash

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { fileURLToPath } from "url";
 import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],  resolve: {
-  alias: {
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
