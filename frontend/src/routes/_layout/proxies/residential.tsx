42ed4665ff631cff2bda8c17adf07f9e5ece0613
cobalt admin <nik@cobaltdata.net>
2025-02-10 18:26:09 +0000

    res

---
+++ frontend/src/routes/_layout/proxies/residential.tsx
@@ -0,0 +1,53 @@
+import {
+    Container,
+    Heading,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+  } from "@chakra-ui/react";
+  import { useQueryClient } from "@tanstack/react-query";
+  import { createFileRoute } from "@tanstack/react-router";
+  
+  import ProxySettings from "../../components/Proxy/ProxySettings";
+  import ProxyUsage from "../../components/Proxy/ProxyUsage";
+  import ProxyBilling from "../../components/Proxy/ProxyBilling";
+  
+  const tabsConfig = [
+    { title: "Settings", component: ProxySettings },
+    { title: "Usage", component: ProxyUsage },
+    { title: "Billing", component: ProxyBilling },
+  ];
+  
+  export const Route = createFileRoute("/_layout/residential-proxy")({
+    component: ResidentialProxy,
+  });
+  
+  function ResidentialProxy() {
+    const queryClient = useQueryClient();
+    const currentUser = queryClient.getQueryData(["currentUser"]);
+  
+    return (
+      <Container maxW="full">
+        <Heading size="lg" textAlign={{ base: "center", md: "left" }} py={12}>
+          Residential Proxy Management
+        </Heading>
+        <Tabs variant="enclosed">
+          <TabList>
+            {tabsConfig.map((tab, index) => (
+              <Tab key={index}>{tab.title}</Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {tabsConfig.map((tab, index) => (
+              <TabPanel key={index}>
+                <tab.component />
+              </TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
+      </Container>
+    );
+  }
+  
