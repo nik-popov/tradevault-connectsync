009f5f07ecb4b18bf3b8f5c5f610a309f6eb70cf
cobalt admin <nik@cobaltdata.net>
2025-02-11 20:01:13 +0000

    serp

--- frontend/src/components/PromoSERP.tsx
+++ frontend/src/components/PromoSERP.tsx
@@ -79,6 +79,18 @@ const PromoSERP: React.FC = () => {
                 Access structured and real-time search data with flexible API integration.
               </Text>
             </Box>
+
+            <Box bg="blue.800" color="gray.300" borderRadius="md" p={5} boxShadow="lg" textAlign="left" display="flex" flexDirection="column" justifyContent="center">
+              <Heading as="h2" size="md" fontWeight="light" color="gray.100">
+                Start Your Free Trial Today!
+              </Heading>
+              <Text fontSize="xs" my={2} color="gray.300">
+                Get full access to our Search API with a 7-day free trial. No credit card required!
+              </Text>
+              <Button bg="blue.600" color="gray.100" _hover={{ bg: "blue.500" }} variant="solid" onClick={() => navigate('/signup')} size="sm">
+                Sign Up for Free
+              </Button>
+            </Box>
           </Grid>
         </Box>
 
