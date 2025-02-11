3cf47247400c1a76ece7273c0e82c2650618203a
cobalt admin <nik@cobaltdata.net>
2025-02-11 10:11:38 +0000

    rec

--- frontend/src/routes/_layout/proxies/datacenter.tsx
+++ frontend/src/routes/_layout/proxies/datacenter.tsx
@@ -28,7 +28,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { FiSend, FiGithub } from "react-icons/fi";
 
 import PromoContent from "../../../components/PromoContent";
-import ProxyStarted from "../../../components/ProxyStarted";
+import CenterStarted from "../../../components/CenterStarted";
 import ProxySettings from "../../../components/ProxySettings";
 import ProxyUsage from "../../../components/ProxyUsage";
 
@@ -301,7 +301,7 @@ function DatacenterProxy() {
 
   // Define tabs configuration with inline components
   const tabsConfig = [
-    { title: "Get Started", component: <ProxyStarted /> },
+    { title: "Get Started", component: <CenterStarted /> },
     { title: "Endpoints", component: <ProxySettings /> },
     { title: "Usage", component: <ProxyUsage /> },
     { title: "Top-Ups", component: <TopUps /> },
