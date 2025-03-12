5a0c33e687affd558f6be0b3f9ef733c0af27994
nikwif <149531476+nikwif@users.noreply.github.com>
2025-03-12 01:46:14 +0000

    active tab overview

--- frontend/src/routes/_layout/scraping-api/scraping-jobs/$jobId.tsx
+++ frontend/src/routes/_layout/scraping-api/scraping-jobs/$jobId.tsx
@@ -752,7 +752,7 @@ const SearchRowsTab: React.FC<SearchRowsTabProps> = ({ job }) => {
   const [debugMode, setDebugMode] = useState(false);
   const [showFileDetails, setShowFileDetails] = useState(true);
   const [showResultDetails, setShowResultDetails] = useState(false);
-  const [numImages, setNumImages] = useState(4);
+  const [numImages, setNumImages] = useState(0);
   const [hideEmptyRows, setHideEmptyRows] = useState(true);
 
   useEffect(() => {
