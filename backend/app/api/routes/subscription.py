63677a221998e15b3a7e9e6c3edb03bd3ea5cc29
nikwif <149531476+nikwif@users.noreply.github.com>
2025-03-19 17:11:14 +0000

    gg

--- backend/app/api/routes/subscription.py
+++ backend/app/api/routes/subscription.py
@@ -1,18 +1,18 @@
-# # app/api/endpoints/subscription.py
-# from fastapi import APIRouter
-# from app.models import SubscriptionStatus, User
-# from typing import Annotated
-# from fastapi import Depends
-# from app.api.deps import get_current_user
-# router = APIRouter()
+# app/api/endpoints/subscription.py
+from fastapi import APIRouter
+from app.models import SubscriptionStatus, User
+from typing import Annotated
+from fastapi import Depends
+from app.api.deps import get_current_user
+router = APIRouter()
 
-# @router.get("/subscription-status/serp", response_model=SubscriptionStatus)
-# async def get_subscription_status(current_user: Annotated[User, Depends(get_current_user)]):
-#     """
-#     Retrieve the subscription status for the authenticated user.
-#     """
-#     return SubscriptionStatus(
-#         hasSubscription=current_user.has_subscription,
-#         isTrial=current_user.is_trial,
-#         isDeactivated=current_user.is_deactivated
-#     )
+@router.get("/subscription-status/serp", response_model=SubscriptionStatus)
+async def get_subscription_status(current_user: Annotated[User, Depends(get_current_user)]):
+    """
+    Retrieve the subscription status for the authenticated user.
+    """
+    return SubscriptionStatus(
+        hasSubscription=current_user.has_subscription,
+        isTrial=current_user.is_trial,
+        isDeactivated=current_user.is_deactivated
+    )
