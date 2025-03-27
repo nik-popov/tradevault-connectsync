8e50eeddc5d9506c5abb50d9e2501c5e9d43e0ac
nikwifhat <apis.popov@gmail.com>
2025-03-27 21:11:51 +0000

    g

--- frontend/src/routes/_layout/scraping-api/google-serp.tsx
+++ frontend/src/routes/_layout/scraping-api/google-serp.tsx
@@ -2,17 +2,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Container, Flex, Text, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import ProtectedComponent from "../../../components/ProtectedComponent"; // Adjust the import path
-import OverviewGSerp from "../../../components/OverviewGSerp";
-import LogsGSerp from "../../../components/LogsGSerp";
 import PlaygroundGSerp from "../../../components/PlaygroundGSerp";
 import ApiKeyGSerp from "../../../components/ApiKeyGSerp";
-import WhitelistGSerp from "../../../components/WhitelistGSerp";
 
 const GoogleSerpPage = () => {
-  const tabsConfig = [
-    // { title: "Overview", component: () => <OverviewGSerp /> },
+  const TabsConfig = [
     { title: "API Keys", component: () => <ApiKeyGSerp /> },
-    { title: "Playground", component: () => <PlaygroundGSerp /> },
+    { title: "Playground", component: () => <PlaygroundGSerp /> }
   ];
 
   return (
@@ -24,12 +20,12 @@ const GoogleSerpPage = () => {
         </Flex>
         <Tabs>
           <TabList>
-            {tabsConfig.map((tab, index) => (
+            {TabsConfig.map((tab, index) => (
               <Tab key={index}>{tab.title}</Tab>
             ))}
           </TabList>
           <TabPanels>
-            {tabsConfig.map((tab, index) => (
+            {TabsConfig.map((tab, index) => (
               <TabPanel key={index}>{tab.component()}</TabPanel>
             ))}
           </TabPanels>
