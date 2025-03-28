629a30f75a25a0c031627afffbd26a42f3c46792
nikwifhat <apis.popov@gmail.com>
2025-03-28 16:26:05 +0000

    tabs

--- frontend/src/routes/_layout/settings.tsx
+++ frontend/src/routes/_layout/settings.tsx
@@ -58,7 +58,7 @@ function UserSettings() {
       <Divider my={4} borderColor="gray.200" />
 
       <Tabs colorScheme="blue">
-        <TabList borderBottom="2px solid">
+        <TabList borderBottom="2px solid" borderColor="gray.200">
           {finalTabs.map((tab, index) => (
             <Tab
               key={index}
