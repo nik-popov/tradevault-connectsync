923dd01180b3e5fed8f8d5926d303d25fb2be3e7
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-05 13:51:01 -0400

    Update db.py

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -13,16 +13,9 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next lines
-    # from sqlmodel import SQLModel
-
-    # This works because the models are already imported and registered from app.models
-    # SQLModel.metadata.create_all(engine)
-
     user = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER)
+        select(User.id, User.email, User.is_active, User.is_superuser)
+        .where(User.email == settings.FIRST_SUPERUSER)
     ).first()
     if not user:
         user_in = UserCreate(
