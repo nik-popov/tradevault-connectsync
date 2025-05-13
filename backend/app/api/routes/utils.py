b620221fca803fcc655f0aec860f406348cc04c1
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-13 23:05:14 +0000

    base

--- backend/app/api/routes/utils.py
+++ backend/app/api/routes/utils.py
@@ -1,8 +1,36 @@
+from fastapi import APIRouter, Depends
+from pydantic.networks import EmailStr
 from jinja2 import Environment, FileSystemLoader
+from app.api.deps import get_current_active_superuser
+from app.models import Message
+from app.utils import generate_test_email, send_email
 from app.core.config import settings
 import logging
 
 logger = logging.getLogger(__name__)
+
+router = APIRouter(prefix="/utils", tags=["utils"])
+
+@router.post(
+    "/test-email/",
+    dependencies=[Depends(get_current_active_superuser)],
+    status_code=201,
+)
+def test_email(email_to: EmailStr) -> Message:
+    """
+    Test emails.
+    """
+    email_data = generate_test_email(email_to=email_to)
+    send_email(
+        email_to=email_to,
+        subject=email_data.subject,
+        html_content=email_data.html_content,
+    )
+    return Message(message="Test email sent")
+
+@router.get("/health-check/")
+async def health_check() -> bool:
+    return True
 
 class EmailData:
     def __init__(self, html_content: str, subject: str):
