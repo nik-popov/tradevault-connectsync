950443e5dbabfc0b5b2961665ed660831c7580e7
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-05-13 10:36:31 +0000

    paths main

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout/")({
   component: () => {
-    window.location.href = "/scraping-tools/https-proxy";
+    window.location.href = "/web-scraping-tools/https-proxy";
     return null;
   },
 });
