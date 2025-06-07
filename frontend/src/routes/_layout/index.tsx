fab6d4c092622269888b1344713e423fcd9594a4
nikwifhat <apis.popov@gmail.com>
2025-06-07 02:29:30 +0000

    core

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,8 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout/")({
-  component: () => {
-    window.location.href = "/web-scraping-tools/https-proxy";
-    return null;
-  },
 });
