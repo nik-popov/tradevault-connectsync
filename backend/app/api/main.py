21f567999237599a944806f4259233307bf835b2
nikwifhat <apis.popov@gmail.com>
2025-06-07 08:51:29 +0000

    serpapi

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -12,11 +12,7 @@ api_router.include_router(utils.router)
 api_router.include_router(items.router)
 api_router.include_router(subscription.router)  
 api_router.include_router(checkout.router)
-
-
-# New user_agent routes
-api_router.include_router(
-    user_agent.router)
+api_router.include_router(user_agent.router)
 api_router.include_router(proxy.router)
 
 
