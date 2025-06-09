e75ca75287d6cb30fc31237ba616f50c6c60960a
nikwifhat <apis.popov@gmail.com>
2025-06-09 22:14:54 +0000

    core

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -7,11 +7,6 @@ from app.models import User, UserCreate
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
-# make sure all SQLModel models are imported (app.models) before initializing DB
-# otherwise, SQLModel might fail to initialize relationships properly
-# for more details: https://github.com/fastapi/full-stack-fastapi-template/issues/28
-
-
 def init_db(session: Session) -> None:
     user = session.exec(
         select(User.id, User.email, User.is_active, User.is_superuser)
@@ -22,5 +17,6 @@ def init_db(session: Session) -> None:
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
+            
         )
         user = crud.create_user(session=session, user_create=user_in)
